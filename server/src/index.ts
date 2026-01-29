1d86d4ecda7a853830fc5427caef1d4d46f0ffdd
damzymichael <105460672+damzymichael@users.noreply.github.com>
2026-01-30 00:11:52 +0100

    fix server host error

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ fastify.register(
   { prefix: "/api" },
 )
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
